1e2c84dbd116b6f2e732d2a1baca9ed545ddb3a6
Agustín Javier Orlando <agusavior@gmail.com>
2022-08-23 16:18:46 -0400

    Add py.typed file

--- stubs/ppm/telegram.pyi
+++
@@ -1,9 +0,0 @@
-from _typeshed import Incomplete
-
-ADMIN_CHAT_IDS: Incomplete
-
-class Telegram:
-    @classmethod
-    def set_token(cls, token: str): ...
-    @classmethod
-    def send_message(cls, text: str): ...
